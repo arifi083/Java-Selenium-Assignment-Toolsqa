cad2db3840c0f3a1592344e16dda8b2a68177820
arifi083 <atifarif083@gmail.com>
2023-05-27 04:04:01 -0700

    something added

--- src/test/java/JunitHandsOn.java
+++ src/test/java/JunitHandsOn.java
@@ -1,11 +1,10 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.apache.commons.io.FileUtils;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.interactions.Actions;
@@ -14,9 +13,15 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.io.File;
+import java.io.IOException;
 import java.nio.file.attribute.AclEntry;
+import java.text.SimpleDateFormat;
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+
 
 public class JunitHandsOn {
     WebDriver driver;
@@ -26,17 +31,13 @@ public class JunitHandsOn {
     @Before
     public void SetUp(){
 
-        System.setProperty("webdriver.chrome.driver","./src/test/resources/chromedriver_win32/chromedriver.exe");
-        ChromeOptions chromeOptions=new ChromeOptions();
-        chromeOptions.addArguments("--headed");
-        driver= new ChromeDriver();
-        //maximize window size
+        WebDriverManager.chromedriver().setup();
+        driver=new ChromeDriver();
         driver.manage().window().maximize();
-        //timeout
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
     }
 
-   // @Test
+    @Test
     public void getTitle() throws InterruptedException {
         //open a new url in browser
         driver.get("https://demoqa.com");
@@ -47,7 +48,7 @@ public class JunitHandsOn {
 
     }
 
-    //@Test
+    @Test
     public void verifyImage(){
         //open a new url in browser
         driver.get("https://demoqa.com");
@@ -58,7 +59,7 @@ public class JunitHandsOn {
         Assert.assertTrue(imageStatus);
     }
 
-   //@Test
+    @Test
     public void registration() throws InterruptedException {
         //open the url
         driver.get("https://demoqa.com/text-box");
@@ -79,7 +80,7 @@ public class JunitHandsOn {
 
 
     }
-   //@Test
+    @Test
     public void doubleClick() throws InterruptedException {
         driver.get("https://demoqa.com/buttons");
         WebElement doubleClickElement = driver.findElement(By.id("doubleClickBtn"));
@@ -90,7 +91,24 @@ public class JunitHandsOn {
         Assert.assertTrue(doubleClickMsg.contains("You have done a double click"));
 
     }
-    //@Test
+
+    public void btnClick(){
+        driver.get("https://demoqa.com/buttons");
+        List<WebElement> button= driver.findElements(By.cssSelector(("[type=button]")));
+        Actions action =new Actions(driver);
+        action.doubleClick(button.get(1)).perform();
+        action.contextClick(button.get(2)).perform(); // for mousehover replace contextClick/ doubleclick by obj.moveToElement
+        action.click(button.get(3)).perform();
+
+        String actual_message=driver.findElement(By.id("doubleClickMessage")).getText();
+        String actual_message2=driver.findElement(By.id("rightClickMessage")).getText();
+        String expected_message= "You have done a double click";
+        Assert.assertTrue(actual_message.contains(expected_message));
+        Assert.assertTrue(actual_message.contains(expected_message));
+    }
+
+
+    @Test
     public void multipleButtons() throws InterruptedException {
         driver.get("https://demoqa.com/buttons");
         //List<WebElement> multipleButtons1 = driver.findElement(By.cssSelector("[type=button]"));
@@ -102,7 +120,7 @@ public class JunitHandsOn {
         Assert.assertTrue(doubleClickMsg.contains("You have done a double click"));
 
     }
-   // @Test
+    @Test
     public void rightClickBtn(){
         driver.get("https://demoqa.com/buttons");
         WebElement rightClick= driver.findElement(By.id("rightClickBtn"));
@@ -113,7 +131,7 @@ public class JunitHandsOn {
         Assert.assertTrue(rightClickMsg.contains("You have done a right click"));
         System.out.println("Right Click text" + rightClickMsg);
     }
-   // @Test
+    @Test
     public void acceptCookies() throws InterruptedException {
         driver.get("https://cleanerdhaka.ideascale.com/a/community/login");
        WebElement cookies = driver.findElement(By.xpath("//button[@class='btn btn-primary flex-fill json-link']"));
@@ -121,7 +139,7 @@ public class JunitHandsOn {
         actions.click(cookies).build().perform();
         Thread.sleep(1000);
     }
-   // @Test
+    @Test
     public void handleAlert() throws InterruptedException {
         driver.get("https://demoqa.com/alerts");
         driver.findElement(By.id("confirmButton")).click();
@@ -133,7 +151,21 @@ public class JunitHandsOn {
        Assert.assertTrue(cancelText.contains("You selected Cancel"));
 
     }
-   // @Test
+
+    @Test
+    public void selectDate() throws InterruptedException {
+        driver.get("https://demoqa.com/date-picker");
+        WebElement date= driver.findElement(By.id("datePickerMonthYearInput"));
+        Thread.sleep(6000);
+        date.sendKeys(Keys.CONTROL+"a",Keys.BACK_SPACE);
+        Thread.sleep(5000);
+        date.sendKeys("09/09/2023");
+        Thread.sleep(5000);
+        date.sendKeys(Keys.ENTER);
+    }
+
+
+   @Test
     public void selectDropDown() throws InterruptedException {
         driver.get("https://demoqa.com/select-menu");
         Select colors = new Select(driver.findElement(By.id("oldSelectMenu")));
@@ -151,7 +183,7 @@ public class JunitHandsOn {
             System.out.println("it is not multiple ");
         }
     }
-   // @Test
+    @Test
     public void selectMultipleDropdwon() throws InterruptedException {
         driver.get("https://demoqa.com/select-menu");
         Select cars = new Select(driver.findElement(By.id("cars")));
@@ -189,14 +221,68 @@ public class JunitHandsOn {
 
         Thread.sleep(3000);
 
+    }
+    @Test
+    public void windowHandleTab() throws InterruptedException {
+        driver.get("https://demoqa.com/links");
+        driver.findElement(By.id("simpleLink")).click();
+        Thread.sleep(3000);
+        ArrayList<String>windowHandle= new ArrayList<>(driver.getWindowHandles());
+        driver.switchTo().window(windowHandle.get(1));
+        String childTab = driver.getTitle();
+        System.out.println("New tab title assertion" +childTab);
+        Assert.assertTrue(childTab.contains("ToolsQA"));
+        boolean imagePresent = driver.findElement(By.xpath("//img[@alt='Selenium Online Training']")).isDisplayed();
+        Assert.assertEquals(true,imagePresent);
+        System.out.println("image status" + imagePresent);
+        driver.close();
 
+        driver.switchTo().window(windowHandle.get(0));
+        String parentTab = driver.getTitle();
+        System.out.println("parent title assertion" +parentTab);
+        Assert.assertTrue(parentTab.contains("ToolsQA"));
 
+    }
+  @Test
+    public void webTable(){
+        driver.get("https://demoqa.com/webtables");
+        WebElement table = driver.findElement(By.className("rt-tbody"));
+        List<WebElement>allRows = driver.findElements(By.className("rt-tr"));
+        System.out.println(allRows);
+
+        int i = 0;
+        for(WebElement row:allRows){
+            List<WebElement>allColums = driver.findElements(By.className("rt-td"));
+
+            for(WebElement col:allColums){
+                i++;
+                System.out.println("colums values ["+ i + "]"+col.getText());
+            }
+        }
+    }
+    @Test
+    public void file() throws InterruptedException {
+        driver.get("https://demoqa.com/upload-download");
+        WebElement uploadFile = driver.findElement(By.id("uploadFile"));
+        uploadFile.sendKeys("C:/Users/Arif/Downloads/sampleFile.jpeg");
+        Thread.sleep(5000);
     }
 
 
+    @Test
+    public void takeScreenShot() throws IOException {
+        driver.get("https://demoqa.com");
+        File screenshotFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        String time = new SimpleDateFormat("dd-MM-yyyy-hh-mm-ss-aa").format(new Date());
+        String fileWithPath = "./src/test/resources/screenshots/" + time + ".png";
+        File DestFile = new File(fileWithPath);
+        FileUtils.copyFile(screenshotFile, DestFile);
+
+    }
 
    @After
     public void closeBrowser(){
+
         driver.close();
     }
 }
